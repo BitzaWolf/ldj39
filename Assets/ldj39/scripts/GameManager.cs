5ccf265506349dc5177bbd09d8215d134328118e
Anthony 'Bitzawolf' Pepe <bitzah@gmail.com>
2017-07-29 17:50:53 -0700

    Added GameManager

--- Assets/ldj39/scripts/GameManager.cs
+++ Assets/ldj39/scripts/GameManager.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm = null;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private GameManager() { } // private to force singleton
+
+    /******************
+     * RUB A DUB PUBS *
+     ******************/
+    [Header("Do not destroys")]
+    public GameObject go_MainTower;
+    public GameObject go_EventSystem;
+    public GameObject cam;
+    public MainTower MainTower;
+
+    void Start ()
+    {
+        if (gm == null)
+            gm = this;
+
+        DontDestroyOnLoad(go_MainTower);
+        DontDestroyOnLoad(go_EventSystem);
+        DontDestroyOnLoad(cam);
+        
+        SceneManager.LoadScene("Level01");
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
